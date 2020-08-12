94105370def79ac7fb499b189f237d382d458aa8
Gustavo Oliveira <62312964+oliveiraagu1@users.noreply.github.com>
2020-08-11 22:25:28 -0300

    Contagem de anos que precisa

---
+++ Altura/script.js
@@ -0,0 +1,11 @@
+let alturaJoao = 175
+let alturaPedro = 160
+let anos = 0 
+
+while(alturaJoao > alturaPedro) {
+    alturaJoao = alturaJoao + 1
+    alturaPedro = alturaPedro + 3
+    anos = anos + 1
+}
+
+alert(`Pedro levou ${anos} anos, para ser mais alto que João`);
