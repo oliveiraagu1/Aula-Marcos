e4b85a968637a4a2725e32a2ac6c811c07e2dbed
Gustavo Oliveira <62312964+oliveiraagu1@users.noreply.github.com>
2020-08-11 22:26:37 -0300

    Faz uma salada de frutas

---
+++ Salada/Script.js
@@ -0,0 +1,9 @@
+let fruta = ''
+let frutaPrincipal = 'cereja'
+
+while(fruta !== frutaPrincipal) {
+
+  fruta = prompt("Qual fruta você deseja adicionar?")
+
+}
+alert("Sua salada de frutas está pronta")
