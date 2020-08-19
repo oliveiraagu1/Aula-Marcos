72a0a3e82be652552b3beaec6cefd9fd4fde7085
Gustavo Oliveira <62312964+oliveiraagu1@users.noreply.github.com>
2020-08-18 22:44:46 -0300

    Calculando os metros que falta para chegar

---
+++ Everest/script.js
@@ -0,0 +1,19 @@
+let meta = 3488
+let subida_diaria = 0
+
+
+function subir() {
+    for (let dias = 0; dias < 8; dias++) {
+        subida_diaria = Number(prompt(`Faltam ${meta} metros para o cume. Quantos metros você subiu hoje?`))
+        meta = meta - subida_diaria
+
+        if (meta < 0 && dias < 8) {
+            alert(`parabéns! Você atingiu o cume em ${dias} dias!`)
+            break;
+        }
+    }
+
+    alert(`Você deve voltar, pois corre risco de ficar sem oxigênio.`)
+}
+
+subir()
